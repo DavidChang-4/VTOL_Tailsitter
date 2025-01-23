cc3f07287481a2bff27491d6e13f9a8d1df811d4
David Chang <dc4266@princeton.edu>
2025-01-23 02:05:53 -0500

    Servo motor and left flight motor communication implemented

--- Flight_Controller/Flight_Controller.ino
+++ Flight_Controller/Flight_Controller.ino
@@ -3,30 +3,68 @@
 // Micro Servos
 Servo servoLeft;
 Servo servoRight;
-int pinServoLeft = 2;
-int pinServoRight = 3;
+const int servoLeftPin = 4;
+const int servoRightPin = 5;
 
-// Flight Motors
-// pinMotorLeft = D6;
+// RadioMaster Reading
+const int inputJoyRX = 2; // Interupt-capable pin
+volatile unsigned long pwmRXStart = 0;
+volatile unsigned long pwmRXHighTime = 0;
+volatile unsigned long pwmRXLowTime = 0;
+
+// ESC Motors
+Servo escLeft;
+const int escLeftPin = 9;
+const int MIN_SIGNAL = 1200; // Minimum throttle (us)
+const int MAX_SIGNAL = 1900; // Maximum throttle (us)
 
 void setup() {
-  // PWM
-  // pinMode(pinMotorLeft, OUTPUT);
+  // RadioMaster Reading
+  pinMode(inputJoyRX, INPUT);
+  attachInterrupt(digitalPinToInterrupt(inputJoyRX), handleJoyRX, CHANGE);
 
-  // Servos
-  servoLeft.attach(pinServoLeft);
-  servoRight.attach(pinServoRight);  // attaches the servo on pin 9 to the servo object
+  // ESC Motors
+  escLeft.attach(escLeftPin);
+  escLeft.writeMicroseconds(MIN_SIGNAL);
+
+  // Micro Servos
+  servoLeft.attach(servoLeftPin);
+  servoRight.attach(servoRightPin);  // attaches the servo on pin 9 to the servo object
+
+  delay(1000);
+}
+
+void handleJoyRX() {
+  if (digitalRead(inputJoyRX) == HIGH) {
+    pwmRXStart = micros(); // Record the time when the signal goes HIGH
+  } else {
+    pwmRXHighTime = micros() - pwmRXStart; // Calculate HIGH duration
+    pwmRXLowTime = pwmRXStart - micros(); // Calculate LOW duration
+  }
 }
 
 void loop() {
-  // Motor Right: Write
-  // analogWrite(pinMotorLeft, 20);
+  // ==========================
+  // RadioMaster: read volatile values
+  // ==========================
+  noInterrupts(); // Needed to read volatile variables safely
+  unsigned long highTimeRX = pwmRXHighTime;
+  unsigned long lowTimeRX = pwmRXLowTime;
+  interrupts();
+  
+  // ==========================
+  // ESC Motors: Convert radio reading + Write
+  // ==========================
+  highTimeRX = map(highTimeRX, 983, 1998, 0, 100);
+  escLeft.writeMicroseconds(map(highTimeRX, 60, 100, MIN_SIGNAL, MAX_SIGNAL));
 
-  // Left Servo: Write
-  servoLeft.write(180);                  // sets the servo position according to the scaled value
+
+
+
+  // ==========================
+  // Micro Servos: Write angle
+  // ==========================
+  servoLeft.write(180);
   servoRight.write(180);
-  delay(10000);                           // waits for the servo to get there
-  servoLeft.write(0);                  // sets the servo position according to the scaled value
-  servoRight.write(0);
-  delay(10000);  
 }
+
